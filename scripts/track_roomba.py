0a42e9fb4504a43a7832a77b1439d10912c0b0c4
Levi Burner <leviburner@gmail.com>
2018-05-23 17:13:03 -0400

    Expand track roomba with basic search and recover behaviour

--- scripts/track_roomba.py
+++ scripts/track_roomba.py
@@ -2,6 +2,7 @@
 import sys
 import rospy
 from iarc7_msgs.msg import OdometryArray
+from iarc7_msgs.msg import RoombaDetectionFrame
 import actionlib
 import tf2_ros
 from iarc7_motion.msg import QuadMoveGoal, QuadMoveAction
@@ -35,25 +36,73 @@ def track_roomba_land():
     if rospy.is_shutdown(): return
     rospy.logwarn("Takeoff success: {}".format(client.get_result()))
 
+    goal = QuadMoveGoal(movement_type="velocity_test", x_velocity=0.0, y_velocity=0.0, z_position=1.5)
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
     rospy.sleep(2.0)
+    client.cancel_goal()
+    rospy.logwarn("Dont ascending")
 
-    # change element in array to test diff roombas
-    roomba_id = roomba_array.data[3].child_frame_id 
-    roomba_id = roomba_id [0:len(roomba_id)-10]
+    goal = QuadMoveGoal(movement_type="velocity_test", x_velocity=1.0, y_velocity=0.0, z_position=1.5)
+    # Sends the goal to the action server.
+    client.send_goal(goal)
 
-    # Test tracking
-    goal = QuadMoveGoal(movement_type="track_roomba", frame_id=roomba_id, 
-        time_to_track=5.0, x_overshoot=.5, y_overshoot=.5)
+    search_start_time = rospy.Time.now()
+    rate = rospy.Rate(30)
+    roomba_detected = False
+    while True:
+        if rospy.Time.now() - search_start_time > rospy.Duration(3.0):
+            rospy.loginfo("Searching for Roomba timed out")
+            break
+        elif len(roomba_detections_array) > 0:
+            roomba_detected = True
+            rospy.loginfo("FOUND ROOMBA")
+            break
+        rate.sleep()
+
+    client.cancel_goal()
+    rospy.logwarn("Translation to roomba canceled")
+
+    if roomba_detected:
+        # change element in array to test diff roombas
+        roomba_id = roomba_array.data[0].child_frame_id 
+        roomba_id = roomba_id [0:len(roomba_id)-10]
+
+        # Test tracking
+        goal = QuadMoveGoal(movement_type="track_roomba", frame_id=roomba_id, 
+            time_to_track=10.0, x_overshoot=0.0, y_overshoot=0.0)
+        # Sends the goal to the action server.
+        client.send_goal(goal)
+        # Waits for the server to finish performing the action.
+        client.wait_for_result()
+        rospy.logwarn("Track Roomba success: {}".format(client.get_result()))
+    else:
+        rospy.logerr("Roomba not found while searching, returning")
+
+    goal = QuadMoveGoal(movement_type="velocity_test", x_velocity=-0.5, y_velocity=0.0, z_position=1.5)
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+    rospy.sleep(3.0)
+    client.cancel_goal()
+    rospy.logwarn("Translation to roomba canceled")
+
+    goal = QuadMoveGoal(movement_type="land")
     # Sends the goal to the action server.
     client.send_goal(goal)
     # Waits for the server to finish performing the action.
     client.wait_for_result()
     if rospy.is_shutdown(): return
-    rospy.logwarn("Track Roomba success: {}".format(client.get_result()))
+    rospy.logwarn("Land success: {}".format(client.get_result()))
 
 def _receive_roomba_status(data):
     global roomba_array
     roomba_array = data
+
+roomba_detections_array = []
+def _receive_roomba_detections(data):
+    global roomba_detections_array
+    roomba_detections_array = data.roombas
 
 if __name__ == '__main__':
     roomba_array = []
@@ -62,5 +111,8 @@ if __name__ == '__main__':
     rospy.init_node('track_roomba_abstract')
     _roomba_status_sub = rospy.Subscriber('roombas',
                      OdometryArray, _receive_roomba_status)
+    _roomba_vision_sub = rospy.Subscriber('detected_roombas',
+                                          RoombaDetectionFrame,
+                                          _receive_roomba_detections)
     track_roomba_land()
     rospy.spin()
